9697304f85b28a7ed9c73fce9c70136e5404fe3b
pires-cpu <r06717880@gmail.com>
2025-05-16 14:59:03 -0300

    modificação do 1

--- atividade numero um/src/atividade/numero/um/AtividadeNumeroUm.java
+++ atividade numero um/src/atividade/numero/um/AtividadeNumeroUm.java
@@ -15,7 +15,9 @@ public class AtividadeNumeroUm {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("teste");
+        
     }
     
 }
